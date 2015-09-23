8e21acc98a9788e89c1d6b72d6da2012d6b1e512
jbegley <jbegley@users.noreply.github.com>
2015-09-22 23:45:09 -0700

    Update cachematrix.R
    change comment wording

--- cachematrix.R
+++ cachematrix.R
@@ -9,11 +9,11 @@ library("MASS")
 
 ## makeCacheMatrix implements a cache for the inverse of the
 ## matrix input as a function argument.  It returns a  4 element
-## list of functions that: [1] set a new value for the matrix (i.e.,
-## values for the matrix elements), and therefore clearing the
-## cached inverse; [2] get (return) the stored matrix;
-## [3] set the stored matrix to another value (thus obliterating the
-## cached inverse); and [4] get (return) the stored (cached) inverse
+## list of functions that: [1] sets a new value for the matrix (i.e.,
+## values for the matrix elements), and thereby clearing the
+## cached inverse; [2] gets (returns) the stored matrix;
+## [3] sets the stored matrix to another value (and obliterates the
+## cached inverse); and [4] gets (returns) the stored (cached) inverse
 ## (if available).
 ## Lexical scoping provides non-volatile storage.
 
